d5a6b3e7d3b565ac17acb12c5d54a56fd0d050ee
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-10-24 13:46:34 -0400

    made new MVC: category, Views for category, events, users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :events
+  resources :users, only: [:show, :new, :create, :update, :edit]
+  resources :events, only: [:new, :create, :update, :edit]
+  resources :categories, only: :index
+
   resources :sessions, only:[:new, :create]
+
 end
