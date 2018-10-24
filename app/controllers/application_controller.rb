85eedc71cefdea8a6885e93ac23196e2cde3cf33
github username <github email address>
2018-10-24 14:43:05 -0400

    create_event before action in application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,10 @@ class ApplicationController < ActionController::Base
   #     @user = User.find(session[:user_id])
   #     redirect_to @user
   #   end
-  # @message = flash[:message]
-  # @errors = flash[:errors]
-#   @logged_in = !!@user
-# end
+  #     @message = flash[:message]
+  #     @errors = flash[:errors]
+  #     @logged_in = !!@user
+  # end
 
 
 #this method matches user in the database to user that is signing in
@@ -34,8 +34,6 @@ class ApplicationController < ActionController::Base
 
 
   #authorizes user that is logging in is the same user currently signed in
-=======
-
   def authorized?(user_id)
     logged_in_user_id == user_id
   end
