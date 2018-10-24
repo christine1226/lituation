36933786e99a4db3395eeae494d5e63a02d32d40
YANA <30638175+YanaNirvana@users.noreply.github.com>
2018-10-24 14:16:09 -0400

    Revert "Revert "Christines branch""

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :users, only: [:show, :new, :create, :update, :edit]
   resources :events, only: [:new, :create, :update, :edit]
   resources :categories, only: :index
 
-  resources :sessions, only:[:new, :create]
+  #sessions paths
+  get '/signup', to: 'users#new'
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 
 end
