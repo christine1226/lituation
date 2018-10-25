0560b08b635cb43d4ffdf4b6c228009a77badd63
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-10-25 11:21:06 -0400

    root homepage done, needs to be 'cool'; debugged so that now all pages work; authentication and authorization works

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,11 @@ Rails.application.routes.draw do
   resources :events, only: [:new, :create, :update, :edit]
   resources :categories, only: [:index]
 
-  get '/users/:id/create_event/new', to: "users#new_event", as: "create_new_event"
-  post '/users/:id/create_event', to: "users#create_event", as: "create_event"
-  get 'users/:id/create_event/:id', to: "users#show", as: "create_user_event"
-
   #sessions paths
-
+  get '/', to: 'home#page'
   get '/signup', to: 'users#new'
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#destroy"
 
 end
