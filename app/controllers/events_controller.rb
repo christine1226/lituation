074bdabb9c70e2a4ea18da01d5fc1d2d25cd9f3e
YanaNirana <30638175+YanaNirvana@users.noreply.github.com>
2018-10-25 15:02:23 -0400

    events#new works again! timezone still messy ._.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,14 +4,16 @@ class EventsController < ApplicationController
 
   def new
     @event = Event.new
-    @users = User.all
+    @user = logged_in_user_id
     @categories = Category.all
   end
 
   def create
     @event = Event.new(event_params)
-    # @event[:start_datetime].asctime.in_time_zone('Eastern Time (US & Canada)')
-    # @event[:end_datetime].asctime.in_time_zone('Eastern Time (US & Canada)')
+    @event.user_id = logged_in_user_id
+    # @event.start_datetime = @event[:start_datetime].asctime.in_time_zone('Eastern Time (US & Canada)')
+    # @event.end_datetime = @event[:end_datetime].asctime.in_time_zone('Eastern Time (US & Canada)')
+
     if @event.save
       redirect_to categories_path
     else
