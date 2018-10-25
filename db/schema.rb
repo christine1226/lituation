5f68724bfe0723ebb6b1a33ae8dae8bf747548a1
Kyle George <k.george21@outlook.com>
2018-10-25 14:15:29 -0400

    corners rounded

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_204615) do
+ActiveRecord::Schema.define(version: 2018_10_24_172706) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -20,16 +20,17 @@ ActiveRecord::Schema.define(version: 2018_10_24_204615) do
 
   create_table "events", force: :cascade do |t|
     t.integer "user_id"
+    t.integer "category_id"
     t.text "content"
     t.string "event_name"
     t.string "address"
     t.string "picture"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "category_id"
     t.datetime "start_datetime"
     t.datetime "end_datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_events_on_category_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
